04aae472927bc48a91a0e865c37ea99a40bf8c6e
mohhaidir <khoiruljmbr@gmail.com>
2020-07-07 05:21:59 +0700

    add cors on server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+const router = require("./routers");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+app.use(router);
 
-const dbConfig = require("./config/database.config.js");
-const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
 mongoose
   .connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -18,9 +21,6 @@ mongoose
     console.log("Can't Connect to Database!");
     process.exit();
   });
-
-const router = require("./routers");
-app.use(router);
 
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT : ${PORT}`);
