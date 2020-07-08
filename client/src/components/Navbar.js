c63d5a324e82f6922a3d4828c12b75aa542fd516
mohhaidir <khoiruljmbr@gmail.com>
2020-07-08 14:45:55 +0700

    edit layout navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Form, FormControl, Button, Navbar } from "react-bootstrap";
+import { Nav, Button, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Header() {
   return (
-    <>
-      <Navbar
-        style={{
-          backgroundImage: "linear-gradient(to bottom right, #00838F, #B2EBF2)"
-        }}
-        variant="light"
-      >
-        <Navbar.Brand style={styles.text}>TODO's App</Navbar.Brand>
-        <Nav className="mr-auto">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      style={{
+        backgroundImage: "linear-gradient(to bottom right, #00838F, #B2EBF2)"
+      }}
+      variant="light"
+    >
+      <Navbar.Brand style={styles.text}>TODO's App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto"></Nav>
+        <Nav>
           <Nav.Link>
             <Link to="/">
               <Button variant="outline-dark">Home</Button>
@@ -24,18 +28,14 @@ export default function Header() {
               <Button variant="outline-dark">Todos</Button>
             </Link>
           </Nav.Link>
-        </Nav>
-        <Form inline>
           <Nav.Link>
             <Link to="/addTodo">
               <Button variant="outline-dark">Add More Todo</Button>
             </Link>
           </Nav.Link>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-dark">Search</Button>
-        </Form>
-      </Navbar>
-    </>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
